85dd54491757f370db94df4971e708bee6c8e9bf
Rohan <rohanreddy027@gmail.com>
2024-09-21 18:12:55 +0100

    Completed ex3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let letters = str.split('');
+    let reversedLetters = letters.reverse();
+    let reversedString = "";
 
+    for (let i = 0; i < reversedLetters.length; i++) {
+        reversedString += reversedLetters[i];
+    }
+    return reversedString;
 };
+
+// reverseString("hello there")
 
 // Do not edit below this line
 module.exports = reverseString;
