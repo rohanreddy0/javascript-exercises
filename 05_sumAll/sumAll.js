6f36a6f57b068ea8949495f8adeb62f396b7c393
Rohan <rohanreddy027@gmail.com>
2024-09-21 18:49:13 +0100

    Completed ex5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
+    const sortedInts = [int1, int2];
+    if(!Number.isInteger(int1) || !Number.isInteger(int2)) return "ERROR";
+    if(int1 < 0 || int2 < 0) return "ERROR";
 
+
+    if (int2 < int1) {
+        sortedInts.shift();
+        sortedInts.push(int1);
+    }
+    let sum = 0;
+    for (let i = sortedInts[0]; i <= sortedInts[1]; i++) {
+        sum += i;
+    }
+    return sum
 };
 
 // Do not edit below this line
