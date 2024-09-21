426a1f5be29a52e97806defa7b77c5bf5f2381d9
Rohan <rohanreddy027@gmail.com>
2024-09-21 18:30:44 +0100

    Completed ex4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...removeVals) {
+
+    const newArray = [];
+
+    array.forEach((item) => {
+        if (!removeVals.includes(item)) {
+            newArray.push(item);
+        }
+    })
+
+
+    return newArray;
 };
 
 // Do not edit below this line
