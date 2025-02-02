37556aea0e40eee288f9d70f175e6191b060b383
Rohan <rohanreddy027@gmail.com>
2025-02-02 19:48:37 +0000

    Completed 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
+const palindromes = function (text) {
+    const alphanumerical = "1234567890qwertyuiopasdfghjklzxcvbnm";
+    const cleanedText = text
+        .toLowerCase()
+        .split('')
+        .filter((character) => alphanumerical.includes(character))
+        .join('');
+
+    const reversedText = cleanedText.split('').reverse().join('');
+
+    return cleanedText === reversedText;
 
 };
+
+console.log(palindromes("racecars"));
 
 // Do not edit below this line
 module.exports = palindromes;
